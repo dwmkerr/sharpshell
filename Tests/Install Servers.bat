6e645b7c6e56ca395216aedecc165e3a1e9a5c75
Dave Kerr <dave_kerr-lln@mckinsey.com>
2013-09-13 18:17:49 +0200

    Added the regression tests.

---
+++ Tests/Install Servers.bat
@@ -0,0 +1,5 @@
+..\build\tools\srm.exe install "..\build\samples\countlinesextension.dll" -codebase
+..\build\tools\srm.exe install "..\build\samples\filetimespropertysheet.dll" -codebase
+..\build\tools\srm.exe install "..\build\samples\ReadOnlyFileIconOverlayHandler.dll" -codebase
+..\build\tools\srm.exe install "..\build\samples\TxtThumbnailHandler.dll" -codebase
+pause
---
+++ Tests/Uninstall Servers.bat
@@ -0,0 +1,5 @@
+..\build\tools\srm.exe uninstall "..\build\samples\countlinesextension.dll" 
+..\build\tools\srm.exe uninstall "..\build\samples\filetimespropertysheet.dll"
+..\build\tools\srm.exe uninstall "..\build\samples\ReadOnlyFileIconOverlayHandler.dll"
+..\build\tools\srm.exe uninstall "..\build\samples\TxtThumbnailHandler.dll" -codebase
+pause
