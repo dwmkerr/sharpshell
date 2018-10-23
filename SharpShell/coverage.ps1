b7864a506249c8c6a8ea81fc3d801e059bcf1a00
Dave Kerr <dwmkerr@gmail.com>
2018-10-23 13:09:13 +0100

    build: prefer choco installation for the nunit runner

--- SharpShell/coverage.ps1
+++ SharpShell/coverage.ps1
@@ -1,9 +1,9 @@
 # Install dependencies.
-choco install -y --no-progress opencover.portable codecov
+choco install -y --no-progress opencover.portable codecov nunit-console-runner
 
 # Create an artifacts directory and build the report.
 New-Item -ItemType Directory -Force -Path .\artifacts\coverage
-OpenCover.Console.exe "-target:packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe" "-targetargs:SharpShell.Tests\bin\Release\SharpShell.Tests.dll"  "-filter:+[SharpShell*]* -[SharpShell.Tests   *]*" "-register:user" "-output:artifacts/coverage/coverage.xml"
+OpenCover.Console.exe "-target:nunit3-console.exe" "-targetargs:SharpShell.Tests\bin\Release\SharpShell.Tests.dll"  "-filter:+[SharpShell*]* -[SharpShell.Tests   *]*" "-register:user" "-output:artifacts/coverage/coverage.xml"
 
 # Upload the report to codecov. The CODECOV_TOKEN env var must be set.
 codecov -f .\artifacts\coverage\coverage.xml
