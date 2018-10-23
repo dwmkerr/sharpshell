2b822601b1c5a8b61bacc4d86b160d83c6c3ce6b
Dave Kerr <dwmkerr@gmail.com>
2018-10-23 13:59:35 +0100

    build: ensure the code coverage script is agnostic to the PWD

--- SharpShell/coverage.ps1
+++ SharpShell/coverage.ps1
@@ -3,7 +3,7 @@ choco install -y --no-progress opencover.portable codecov nunit-console-runner
 
 # Create an artifacts directory and build the report.
 New-Item -ItemType Directory -Force -Path .\artifacts\coverage
-OpenCover.Console.exe "-target:nunit3-console.exe" "-targetargs:SharpShell.Tests\bin\Release\SharpShell.Tests.dll"  "-filter:+[SharpShell*]* -[SharpShell.Tests   *]*" "-register:user" "-output:artifacts/coverage/coverage.xml"
+OpenCover.Console.exe "-target:nunit3-console.exe" "-targetargs:$PSScriptRoot\SharpShell.Tests\bin\Release\SharpShell.Tests.dll"  "-filter:+[SharpShell*]* -[SharpShell.Tests   *]*" "-register:user" "-output:$PSScriptRoot\artifacts\coverage\coverage.xml"
 
 # Upload the report to codecov. The CODECOV_TOKEN env var must be set.
-codecov -f .\artifacts\coverage\coverage.xml
+codecov -f "$PSScriptRoot\artifacts\coverage\coverage.xml"
