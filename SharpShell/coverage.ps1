8d834b89faafd739e77597c2bfee1b487b7b132b
Dave Kerr <dwmkerr@gmail.com>
2018-10-23 14:12:50 +0100

    chore: fix issue with coverage.ps1 script root

--- SharpShell/coverage.ps1
+++ SharpShell/coverage.ps1
@@ -2,7 +2,7 @@
 choco install -y --no-progress opencover.portable codecov nunit-console-runner
 
 # Create an artifacts directory and build the report.
-New-Item -ItemType Directory -Force -Path .\artifacts\coverage
+New-Item -ItemType Directory -Force -Path "$PSScriptRoot\artifacts\coverage"
 OpenCover.Console.exe "-target:nunit3-console.exe" "-targetargs:$PSScriptRoot\SharpShell.Tests\bin\Release\SharpShell.Tests.dll"  "-filter:+[SharpShell*]* -[SharpShell.Tests   *]*" "-register:user" "-output:$PSScriptRoot\artifacts\coverage\coverage.xml"
 
 # Upload the report to codecov. The CODECOV_TOKEN env var must be set.
