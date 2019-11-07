1ce307aaf5a077d564402cd387a5598351a5cd42
Dave Kerr <dwmkerr@gmail.com>
2019-11-07 09:08:27 +0800

    refactor: simplify the 'BuildContextMenuItem' logic

--- SharpShell/SharpShell/SharpContextMenu/NativeContextMenuWrapper.cs
+++ SharpShell/SharpShell/SharpContextMenu/NativeContextMenuWrapper.cs
@@ -30,17 +30,14 @@ namespace SharpShell.SharpContextMenu
         /// <param name="itemIndex">The zero-based position at which to insert the first new menu item.</param>
         /// <param name="firstItemId">The first item id.</param>
         /// <param name="toolStripItems">The tool strip menu items.</param>
-        /// <param name="reserved">Reserved, do not use.</param>
         /// <returns>The index of the last item created.</returns>
-        public uint BuildNativeContextMenu(IntPtr hMenu, uint itemIndex, uint firstItemId, ToolStripItemCollection toolStripItems, int reserved = 1)
+        public uint BuildNativeContextMenu(IntPtr hMenu, uint itemIndex, uint firstItemId, ToolStripItemCollection toolStripItems)
         {
-            //  Create an ID counter and position counter.
+            //  Create an ID counter and position counter. The position is provided by the caller. If this is a top level menu item (i.e.
+            //  top level in the shell context menu) then 'position' will be provided by the Shell via an earlier call to IContextMenu::QueryContextMenu.
+            //  When we create submenus, we simply start at position '0'.
             var idCounter = firstItemId;
-            uint positionCounter = 0;
-
-            // We'll insert only outer elements.
-            var insert = Convert.ToBoolean(reserved);
-            if (insert) positionCounter = itemIndex;
+            var positionCounter = itemIndex;
 
             //  Go through every tool strip item.
             foreach (ToolStripItem item in toolStripItems)
@@ -64,17 +61,16 @@ namespace SharpShell.SharpContextMenu
                     continue;
                 }
 
-                //  We successfully created the menu item, so increment the counters.
+                //  We successfully created the menu item, so increment the position and ID counters.
                 indexedCommands.Add(item);
                 idCounter++;
                 positionCounter++;
 
                 //  Have we just built a menu item? If so, does it have child items?
-                var toolStripMenuItem = item as ToolStripMenuItem;
-                if (toolStripMenuItem != null && toolStripMenuItem.HasDropDownItems)
+                if (item is ToolStripMenuItem toolStripMenuItem && toolStripMenuItem.HasDropDownItems)
                 {
-                    //  Create each drop down item.
-                    idCounter = BuildNativeContextMenu(menuItemInfo.hSubMenu, itemIndex, idCounter, toolStripMenuItem.DropDownItems, 0);
+                    //  Create the drop down menu. As this is a submenu, we start at position zero and go from there.
+                    idCounter = BuildNativeContextMenu(menuItemInfo.hSubMenu, 0, idCounter, toolStripMenuItem.DropDownItems);
                 }
             }
 
